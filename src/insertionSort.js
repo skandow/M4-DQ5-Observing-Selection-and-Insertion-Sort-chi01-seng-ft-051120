21c913ecadefbcbde8a7884f0772155595c79272
skandow <kando1sr@gmail.com>
2020-07-20 10:31:59 -0500

    Done.

--- src/insertionSort.js
+++ src/insertionSort.js
@@ -1,4 +1,17 @@
 function insertionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
-}
+  for (i=1; i<arr.length + 1; i++) {
+    let arrayToCheck = arr.slice(0, i)
+    if (arrayToCheck.length !== 1) { 
+    arrayToCheck.sort(function(a, b) {
+      return a - b;
+    }) 
+    }
+    let counter = 0
+    for (j=0; j<arrayToCheck.length; j++) {
+      arr[counter] = arrayToCheck[j]
+      counter += 1
+    }
+    } 
+  }
--- src/selectionSort.js
+++ src/selectionSort.js
@@ -1,4 +1,16 @@
 function selectionSort(arr) {
   // your code here!
   // arr is an array of unsorted integers (i.e. [3, 5, 1])
-}
+  for (i=0; i<arr.length; i++) {
+    let arrayToCheck = arr.slice(i)
+    let m = Math.min(...arrayToCheck)
+    let filteredArray = arrayToCheck.filter(item => item !== m)
+    arr[i] = m 
+    let counter = i + 1
+    for (j=0; j<filteredArray.length; j++) {
+      arr[counter] = filteredArray[j]
+      counter += 1
+    }
+    console.log(arr)
+    } 
+  }
